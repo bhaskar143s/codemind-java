eea2325f852715c6415d3b8268be8a3c89c8d5d1
bhaskar143s <106300895+bhaskar143s@users.noreply.github.com>
2022-10-14 22:32:40 +0530

    import java.util.Scanner; class J{     public static boolean Prime(int n){         if(n<2){             return false;         }         for(int i=2;i<=(int)Math.sqrt(n);i++){             if(n%i==0){                 return false;             }         }         return true;     }     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n,c=0,k;
            n=sc.nextInt();
            int a[]=new int[n];
            for(int i=0;i<n;i++){
                a[i]=sc.nextInt();
            }
               k=sc.nextInt();
            for(int i=0;i<a.length;i++){
                if(Prime(a[i]) && a[i]>=k){
                    c++;
                }
            }
            System.out.println(c);
        }
    }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class J{
+    public static boolean Prime(int n){
+        if(n<2){
+            return false;
+        }
+        for(int i=2;i<=(int)Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n,c=0,k;
+     
+        n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+           k=sc.nextInt();
+        for(int i=0;i<a.length;i++){
+            if(Prime(a[i]) && a[i]>=k){
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
